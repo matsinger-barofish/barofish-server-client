714a80336207b5876604c4d9eacd05f6c6d6c86c
stk346 <stk346@gamil.com>
2023-11-03 09:36:00 +0900

    refactor: 로깅기능 구현중

--- src/main/java/com/matsinger/barofishserver/WebConfig.java
+++ src/main/java/com/matsinger/barofishserver/WebConfig.java
@@ -1,7 +1,9 @@
 package com.matsinger.barofishserver;
 
+import com.matsinger.barofishserver.global.log.LoggingInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -12,5 +14,10 @@ public class WebConfig implements WebMvcConfigurer {
                 "POST",
                 "DELETE").maxAge(3000)
                 .exposedHeaders("Content-Disposition");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoggingInterceptor());
     }
 }
