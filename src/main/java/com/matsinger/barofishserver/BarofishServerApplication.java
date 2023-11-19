2defada02b432661754d4a1b35d804f9958c7962
stk346 <stk346@gamil.com>
2023-11-19 23:48:18 +0900

    faet: 모니터링 기능 구현중

--- src/main/java/com/matsinger/barofishserver/BarofishServerApplication.java
+++ src/main/java/com/matsinger/barofishserver/BarofishServerApplication.java
@@ -2,8 +2,11 @@ package com.matsinger.barofishserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
 
 @EnableScheduling
 @EnableJpaAuditing
@@ -14,4 +17,8 @@ public class BarofishServerApplication {
 		SpringApplication.run(BarofishServerApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
 }
