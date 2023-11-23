b38980ce1705a7f53773abbe83b7661717093acf
stk346 <stk346@gamil.com>
2023-11-23 11:54:07 +0900

    refactor: ProductTastingNoteResponse - productId 필드 추가, 기존 id = tastingNoteId로 변경

--- src/main/java/com/matsinger/barofishserver/domain/tastingNote/dto/ProductTastingNoteResponse.java
+++ src/main/java/com/matsinger/barofishserver/domain/tastingNote/dto/ProductTastingNoteResponse.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class ProductTastingNoteResponse {
 
     private Integer id;
+    private Integer productId;
     private String image;
     private String storeName;
     private Integer originPrice;
@@ -51,7 +52,8 @@ public class ProductTastingNoteResponse {
     }
 
     public void setProductInfo(ProductTastingNoteInquiryDto dto) {
-        this.id = dto.getProductId();
+        this.id = dto.getId();
+        this.productId = dto.getProductId();
         this.storeName = dto.getStoreName();
         this.originPrice = dto.getOriginPrice();
         this.discountPrice = dto.getDiscountPrice();
