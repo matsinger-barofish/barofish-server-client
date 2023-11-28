1f35d395930ac5c20a1446a5b9dc96eaa5a2a7a8
stk346 <stk346@gamil.com>
2023-11-28 13:43:05 +0900

    refactor: 커밋

---
+++ src/main/java/com/matsinger/barofishserver/monitoring/HelloController.java
@@ -0,0 +1,26 @@
+package com.matsinger.barofishserver.monitoring;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+class HelloController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
+    private final RestTemplate restTemplate;
+
+    HelloController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        LOGGER.info("---------Hello method started---------");
+        ResponseEntity<String> responseEntity = this.restTemplate.postForEntity("https://httpbin.org/post", "Hello, Cloud!", String.class);
+        return responseEntity.getBody();
+    }
+}
