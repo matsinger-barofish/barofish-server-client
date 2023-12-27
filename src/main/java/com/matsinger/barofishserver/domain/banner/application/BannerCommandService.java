badba7b1ec8921f6b8bc9270bd2d3c0877476c78
stk346 <stk346@gamil.com>
2023-12-27 09:46:17 +0900

    refactor: BannerController, MainController - 배너 정렬 관련 코드 수정

--- src/main/java/com/matsinger/barofishserver/domain/banner/application/BannerCommandService.java
+++ src/main/java/com/matsinger/barofishserver/domain/banner/application/BannerCommandService.java
@@ -1,11 +1,14 @@
 package com.matsinger.barofishserver.domain.banner.application;
 
 import com.matsinger.barofishserver.domain.banner.domain.Banner;
+import com.matsinger.barofishserver.domain.banner.dto.BannerDto;
 import com.matsinger.barofishserver.domain.banner.repository.BannerRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -34,5 +37,23 @@ public class BannerCommandService {
         } catch (Exception e) {
             return false;
         }
+    }
+
+    @Transactional
+    public List<BannerDto> sortBanners(List<Integer> bannerIds) {
+        List<Banner> banners = bannerRepository.findAll();
+        for (Banner banner : banners) {
+            banner.setSortNo(null);
+        }
+
+        List<BannerDto> bannerDtos = new ArrayList<>();
+        List<Banner> bannersToSort = bannerRepository.findAllByIdIn(bannerIds);
+        int sortIdx = 1;
+        for (Banner banner : bannersToSort) {
+            banner.setSortNo(sortIdx);
+            bannerDtos.add(banner.convert2Dto());
+            sortIdx++;
+        }
+        return bannerDtos;
     }
 }
