70413702b5e1684f6f704c39602bd2c44a8d313c
stk346 <stk346@gamil.com>
2024-01-08 10:38:56 +0900

    refactor: ProductDeliverFeeType - C타입 제거

--- src/main/java/com/matsinger/barofishserver/domain/product/domain/ProductDeliverFeeType.java
+++ src/main/java/com/matsinger/barofishserver/domain/product/domain/ProductDeliverFeeType.java
@@ -2,7 +2,6 @@ package com.matsinger.barofishserver.domain.product.domain;
 
 public enum ProductDeliverFeeType {
 
-    FREE, C_FIX, C_FREE_IF_OVER,
-    FIX, FREE_IF_OVER;
+    FREE, FIX, FREE_IF_OVER;
 }
 
