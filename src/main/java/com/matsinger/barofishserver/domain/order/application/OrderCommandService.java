3f91e85ef4c09698b91dfc19a3a8cff48574ad83
stk346 <stk346@gamil.com>
2024-01-10 16:41:10 +0900

    refactor: 주문할 때 총 주문금액 관련 에러 발생시 백엔드에서 계산한 총 금액 콘솔에 출력하도록 코드 수정

--- src/main/java/com/matsinger/barofishserver/domain/order/application/OrderCommandService.java
+++ src/main/java/com/matsinger/barofishserver/domain/order/application/OrderCommandService.java
@@ -316,6 +316,7 @@ public class OrderCommandService {
     private Integer validateFinalPrice(OrderReq request, int totalOrderPriceContainsDeliveryFee) {
         int finalOrderPrice = totalOrderPriceContainsDeliveryFee - request.getCouponDiscountPrice() - request.getPoint();
         if (finalOrderPrice != request.getTotalPrice()) {
+            log.info("finalOrderPrice = {}", finalOrderPrice);
             throw new BusinessException("총 금액을 확인해주세요.");
         }
         return finalOrderPrice;
