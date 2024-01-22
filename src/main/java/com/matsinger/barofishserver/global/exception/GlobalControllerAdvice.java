4e8397c701c559464ad138034c0767a07132cea2
stk346 <stk346@gamil.com>
2024-01-22 12:24:29 +0900

    refactor: GlobalControllerAdvice - jwtBusinessException 받아주도록 코드 수정

--- src/main/java/com/matsinger/barofishserver/global/exception/GlobalControllerAdvice.java
+++ src/main/java/com/matsinger/barofishserver/global/exception/GlobalControllerAdvice.java
@@ -1,9 +1,10 @@
 package com.matsinger.barofishserver.global.exception;
 
+import com.matsinger.barofishserver.global.filter.MultiAccessRequestWrapper;
+import com.matsinger.barofishserver.jwt.exception.JwtBusinessException;
 import com.matsinger.barofishserver.utils.CustomResponse;
 import com.matsinger.barofishserver.utils.LoggingUtils;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -45,5 +46,20 @@ public class GlobalControllerAdvice implements HandlerInterceptor {
         customResponse.setIsSuccess(false);
         customResponse.setErrorMsg(e.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(customResponse);
+    }
+
+    @ExceptionHandler(value = {JwtBusinessException.class})
+    public ResponseEntity<CustomResponse<Object>> getJwtExpiredMessage(
+            HttpServletRequest request,
+            JwtBusinessException e) throws IOException {
+
+        MultiAccessRequestWrapper wrapRequest = (MultiAccessRequestWrapper) request;
+        loggingUtils.doLog(wrapRequest, e, "warn");
+
+        CustomResponse customResponse = new CustomResponse();
+        customResponse.setIsSuccess(false);
+        customResponse.setCode(e.getCode());
+        customResponse.setErrorMsg(e.getMessage());
+        return ResponseEntity.ok(customResponse);
     }
 }
