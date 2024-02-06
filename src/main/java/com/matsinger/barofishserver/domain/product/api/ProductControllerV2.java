b2bce99da4e6f9b13a7dc366c87891c445734495
stk346 <stk346@gamil.com>
2024-02-06 18:12:08 +0900

    Revert "feat: [searchEngine] api/v2/product/list api 페이지네이션 제거"
    This reverts commit 4a183f627ccf9bdeb2f0e92db2cf59ad95cf2178.

--- src/main/java/com/matsinger/barofishserver/domain/product/api/ProductControllerV2.java
+++ src/main/java/com/matsinger/barofishserver/domain/product/api/ProductControllerV2.java
@@ -25,6 +25,7 @@ import com.matsinger.barofishserver.utils.Common;
 import com.matsinger.barofishserver.utils.CustomResponse;
 import com.matsinger.barofishserver.utils.S3.S3Uploader;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -60,7 +61,7 @@ public class ProductControllerV2 {
     private final S3Uploader s3;
 
     @GetMapping("/list")
-    public ResponseEntity<CustomResponse<List<ProductListDto>>> selectProductListByUserV2(@RequestHeader(value = "Authorization", required = false) Optional<String> auth,
+    public ResponseEntity<CustomResponse<Page<ProductListDto>>> selectProductListByUserV2(@RequestHeader(value = "Authorization", required = false) Optional<String> auth,
                                                                                           @RequestParam(value = "page", defaultValue = "1") Integer page,
                                                                                           @RequestParam(value = "take", defaultValue = "10") Integer take,
                                                                                           @RequestParam(value = "sortby", defaultValue = "RECOMMEND", required = false) ProductSortBy sortBy,
@@ -70,7 +71,7 @@ public class ProductControllerV2 {
                                                                                           @RequestParam(value = "keyword", required = false, defaultValue = "") String keyword,
                                                                                           @RequestParam(value = "storeId", required = false) Integer storeId) {
 
-        CustomResponse<List<ProductListDto>> res = new CustomResponse<>();
+        CustomResponse<Page<ProductListDto>> res = new CustomResponse<>();
 
         
         TokenInfo tokenInfo = jwt.validateAndGetTokenInfo(Set.of(TokenAuthType.ALLOW, TokenAuthType.USER), auth);
@@ -78,7 +79,7 @@ public class ProductControllerV2 {
         Integer userId = tokenInfo != null ? tokenInfo.getId() : null;
 
         PageRequest pageRequest = PageRequest.of(page - 1, take);
-        List<ProductListDto> result = productQueryService.getPagedProductsWithKeyword(
+        Page<ProductListDto> result = productQueryService.getPagedProductsWithKeyword(
                 pageRequest,
                 sortBy,
                 utils.str2IntList(categoryIds),
